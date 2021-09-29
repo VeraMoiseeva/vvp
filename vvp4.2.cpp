f43bc559ebc812a938a94521eca10ef575075c61
VeraMoiseeva <90762231+VeraMoiseeva@users.noreply.github.com>
2021-09-29 09:59:58 +0300

    Add files via upload

---
+++ vvp4.2.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    int d;
+    setlocale(0, "");
+    std::cout << "Введите диаметр окружности d\n ";
+    std::cin >> d;
+    std::cout << "Длина окружности:";
+    std::cout << d * 3.14 << "\n";
+}
