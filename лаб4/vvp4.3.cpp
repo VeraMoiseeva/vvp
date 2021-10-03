c7ace35978f1272d3e31d0809cf5570cb2742553
VeraMoiseeva <90762231+VeraMoiseeva@users.noreply.github.com>
2021-10-03 17:14:56 +0300

    Add files via upload

---
+++ лаб4/vvp4.3.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+int main()
+{
+    float a, b;
+    setlocale(0, "");
+    std::cout << "Введите два числа a и b\n ";
+    std::cin >> a;
+    std::cin >> b;
+    std::cout << "Их среднее арифметическое:";
+    std::cout << (a + b)/2 << "\n";
+}
